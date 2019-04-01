5bd7500b854009ff05f67d6d5416dfbf74eac402
bayshubar <yerkebulan199@gmail.com>
2019-04-02 02:34:40 +0600

    The first view is finished

--- classifications/views.py
+++ classifications/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+        HTTP_200_OK,
+        HTTP_400_BAD_REQUEST,
+    )
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def classify(request):
+
+    return Response({}, status=HTTP_200_OK)
